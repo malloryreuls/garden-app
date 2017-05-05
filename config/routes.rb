c6792092e1d1ebe36d98355fb3edd02792dbcb59
David Bellotti <davidbellotti@Davids-MacBook-Pro.local>
2017-05-05 13:26:32 -0700

    adds a root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root to: 'plants#index'
+
   resources :plants
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
