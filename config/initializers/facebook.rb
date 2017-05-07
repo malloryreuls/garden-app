3e0c14595c5c5ea36254c6a5c664a79e4ac66414
dbellotti <dbellotti@gmail.com>
2017-05-07 13:09:56 -0700

    adds facebook authentication becuase why not

---
+++ config/initializers/facebook.rb
@@ -0,0 +1,5 @@
+# config/initializers/load_facebook.rb
+APP_CONFIG = YAML.load_file("#{Rails.root}/config/facebook.yml")[Rails.env]
+FACEBOOK_APP_ID = APP_CONFIG['app_id']
+FACEBOOK_APP_SECRET = APP_CONFIG['app_secret']
+FACEBOOK_CALLBACK_URL = APP_CONFIG['callback_url']
